144766c21f310590863f875414df6ebddb576cc4
VitaliyOleinik <oleynik624@gmail.com>
2021-03-05 23:00:51 +0600

    first project with java spring framework, use MVC, Initializer,Controller. Also add index.jsp and aboutPage.jsp

---
+++ WorkWithSpringMVC/src/main/java/com/controllers/MainController.java
@@ -0,0 +1,21 @@
+package com.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView indexPage(){
+        ModelAndView mw = new ModelAndView("index");
+        return mw;
+    }
+
+    @RequestMapping(value = "about", method = RequestMethod.GET)
+    public ModelAndView aboutPage(){
+        ModelAndView mw = new ModelAndView("aboutPage");
+        return mw;
+    }
+}
---
+++ WorkWithSpringMVC/target/WorkWithSpringMVC-1.0-SNAPSHOT/WEB-INF/views/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Mi Book
+  Date: 3/5/2021
+  Time: 8:41 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <h1>My first page with Java Spring FrameWork</h1>
+    <a href="/about">ABOUT</a>
+</body>
+</html>
