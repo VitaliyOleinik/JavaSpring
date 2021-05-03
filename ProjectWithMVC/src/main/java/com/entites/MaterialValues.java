d8ca5e1747b6483544278478ffe21c1c60564c7b
VitaliyOleinik <oleynik624@gmail.com>
2021-05-03 13:07:31 +0600

    add entity MaterialValues class

---
+++ ProjectWithMVC/src/main/java/com/entites/MaterialValues.java
@@ -0,0 +1,61 @@
+package com.entites;
+
+public class MaterialValues {
+    private long id;
+    private String name;
+    private int cost;
+    private int total;
+
+    public MaterialValues(long id, String name, int cost, int total) {
+        this.id = id;
+        this.name = name;
+        this.cost = cost;
+        this.total = total;
+    }
+
+    public MaterialValues(long id, String name, int cost) {
+        this(id, name, cost, 0);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    @Override
+    public String toString() {
+        return "MaterialValues{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", cost=" + cost +
+                ", total=" + total +
+                '}';
+    }
+}
