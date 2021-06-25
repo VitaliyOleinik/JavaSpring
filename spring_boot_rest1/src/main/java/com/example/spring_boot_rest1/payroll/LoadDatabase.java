d874b67a8c1d06d523a54053acfc2b6beedeb3aa
VitaliyOleinik <oleynik624@gmail.com>
2021-06-25 18:58:28 +0600

    LoadDatabase

---
+++ spring_boot_rest1/src/main/java/com/example/spring_boot_rest1/payroll/LoadDatabase.java
@@ -0,0 +1,15 @@
+package com.example.spring_boot_rest1.payroll;
+
+@Configuration
+class LoadDatabase {
+
+    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+
+    @Bean
+    CommandLineRunner initDatabase(EmployeeRepository repository) {
+
+        return args -> {
+            log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "burglar")));
+            log.info("Preloading " + repository.save(new Employee("Frodo Baggins", "thief")));
+        };
+    }
